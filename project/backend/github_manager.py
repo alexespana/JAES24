c878c04efec58d21fa4d93ad335aae7b047db9e1
Joaquín Alejandro España Sánchez <alexespsan@uma.es>
2024-05-11 21:08:36 +0200

    ♻️ Change get_builds method to paginate all workflow runs filtering by branch and by number of runs up to 100. Extract features from a buid for a specific repository #1

--- project/backend/github_manager.py
+++ project/backend/github_manager.py
@@ -44,14 +44,58 @@ class GithubManager:
         else:
             return None
 
-    def get_builds(self, owner, repo_name):
+    def get_builds(self, owner, repo_name, branch="main", number_of_builds=101):
+        """
+        This method allows obtaining up to the last 100 workflow runs from a 
+        repository. If the number of workflow runs exceeds the maximun
+        value (100), all repository workflow runs will be returned.
+
+        Args:
+        owner (str): The owner of the repository.
+        repo_name (str): The name of the repository.
+        number_of_builds (int): The number of workflow runs to return.
+
+        Returns:
+        dict: The dictionary containing the workflow runs metadata.
+        """
+        maximum_value = 100
         url = replace_fields(GET_BUILDS, owner, repo_name)
-        response = requests.get(url, headers=self.headers)
-        if response.status_code == 200:
-            return response.json()
+        params = {
+            "per_page": number_of_builds,
+            "branch": branch
+        }
+
+        if number_of_builds > maximum_value:
+            results = {
+                "workflow_runs": []
+            }
+
+            next_page = url
+            
+            while next_page:
+                response = requests.get(next_page, params=params, headers=self.headers)
+                response.raise_for_status()
+
+                data = response.json()
+
+                if "workflow_runs" in data:
+                    results["total_count"] = data["total_count"]
+                    results["workflow_runs"].extend(data["workflow_runs"])
+
+                # Check if there's a next page
+                if 'next' in response.links:
+                    next_page = response.links['next']['url']
+                else:
+                    next_page = None
+
+            return results
         else:
-            return None
-        
+            response = requests.get(url, params=params, headers=self.headers)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                return None
+
     def get_build(self, owner, repo_name, run_id):
         url = replace_fields(GET_BUILD, owner=owner, repo_name=repo_name, run_id=str(run_id))
         response = requests.get(url, headers=self.headers)
