9fee1fa3b81c6f48aafb9ac0fb35ca97a4d34bb8
Joaquín Alejandro España Sánchez <alexespsan@uma.es>
2024-06-03 19:19:26 +0200

    🗃️ Add new table to store Classifiers types. #7

--- project/backend/models.py
+++ project/backend/models.py
@@ -15,4 +15,31 @@ class RepositoryMetadata(db.Model):
     repository = db.Column(db.String(255), primary_key=True)
     branch = db.Column(db.String(255), primary_key=True)
     features_file = db.Column(db.String(255))
+    model_pickle = db.Column(db.String(255))
 
+class LookupModels(db.Model):
+   __tablename__ = 'lookup_models'
+
+   name = db.Column(db.String(255), primary_key=True)
+   description = db.Column(db.String(255))
+
+
+def init_db():
+    db.create_all()  # Creates all tables defined by models
+    if not LookupModels.query.first():
+        # Insert the AI models
+        dt = LookupModels(name='Decision Tree', description='Supervised learning models that split data into branches to make decisions based on feature values.')
+        db.session.add(dt)
+        rd = LookupModels(name='Random Forest', description='Ensemble method combining multiple decision trees to improve accuracy.')
+        db.session.add(rd)
+        lg = LookupModels(name='Linear Regression', description='Models the relationship between dependent and independent variables using a linear equation.')
+        db.session.add(lg)
+        svm = LookupModels(name='Support Vector Machine', description='Classifies data by finding the optimal hyperplane that separates classes.')
+        db.session.add(svm)
+        kn = LookupModels(name='K-Nearest Neighbors', description='Classifies data based on the "k" closest training examples.')
+        db.session.add(kn)
+        nn = LookupModels(name='Neural Network', description='Models complex patterns using layers of interconnected nodes.')
+        db.session.add(nn)
+
+        # Save on DB
+        db.session.commit()
