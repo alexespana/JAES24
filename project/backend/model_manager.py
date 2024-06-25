b3a403e955c9fed253e32c20c3eb7e30772b79b2
Joaquín Alejandro España Sánchez <alexespsan@uma.es>
2024-06-25 23:52:21 +0200

    ✨ Change linear regression model to Logistic Regression and add functionality to calculate the AUC ROC and plot it #9

--- project/backend/model_manager.py
+++ project/backend/model_manager.py
@@ -10,10 +10,20 @@ from utils import print_model_metrics
 from constants import AIMODELS_FOLDER
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import (
+    accuracy_score, precision_score, 
+    recall_score, f1_score, 
+    confusion_matrix, ConfusionMatrixDisplay,
+    roc_auc_score, roc_curve
+)
+from constants import (
+    DT_CLASSIFIER, RF_CLASSIFIER,
+    LR_CLASSIFIER, SVM_CLASSIFIER,
+    KNN_CLASSIFIER, NN_CLASSIFIER
+)
 
 def get_model_path(model_name: str, classifier_type: str) -> str:
     """
@@ -51,17 +61,17 @@ def parse_classifier(classifier: str) -> str:
     """
     model_type = ''
 
-    if classifier == 'Decision Tree':
+    if classifier == DT_CLASSIFIER:
         model_type = 'dt'
-    elif classifier == 'Random Forest':
+    elif classifier == RF_CLASSIFIER:
         model_type = 'rf'
-    elif classifier == 'Linear Regression':
+    elif classifier == LR_CLASSIFIER:
         model_type = 'lr'
-    elif classifier == 'Support Vector Machine':
+    elif classifier == SVM_CLASSIFIER:
         model_type = 'svm'
-    elif classifier == 'K-Nearest Neighbors':
+    elif classifier == KNN_CLASSIFIER:
         model_type = 'knn'
-    elif classifier == 'Neural Network':
+    elif classifier == NN_CLASSIFIER:
         model_type = 'nn'
 
     return model_type
@@ -112,55 +122,60 @@ def train_all(data, pickle_pattern, model_evaluation = False, test_percentage=0.
 
     dt = DecisionTreeClassifier(random_state=seed)
     train(dt, x_train, y_train)
-    with open(get_model_path(pickle_pattern, 'Decision Tree'), 'wb') as file:
+    with open(get_model_path(pickle_pattern, DT_CLASSIFIER), 'wb') as file:
         pickle.dump(dt, file)
 
     rf = RandomForestClassifier(random_state=seed)
     train(rf, x_train, y_train)
-    with open(get_model_path(pickle_pattern, 'Random Forest'), 'wb') as file:
+    with open(get_model_path(pickle_pattern, RF_CLASSIFIER), 'wb') as file:
         pickle.dump(rf, file)
 
-    lr = LinearRegression()
+    lr = LogisticRegression(random_state=seed, max_iter=15000)
     train(lr, x_train, y_train)
-    with open(get_model_path(pickle_pattern, 'Linear Regression'), 'wb') as file:
+    with open(get_model_path(pickle_pattern, LR_CLASSIFIER), 'wb') as file:
         pickle.dump(lr, file)
 
-    svc = SVC(random_state=seed)
+    svc = SVC(random_state=seed, probability=True)
     train(svc, x_train, y_train)
-    with open(get_model_path(pickle_pattern, 'Support Vector Machine'), 'wb') as file:
+    with open(get_model_path(pickle_pattern, SVM_CLASSIFIER), 'wb') as file:
         pickle.dump(svc, file)
 
     knn = KNeighborsClassifier()
     train(knn, x_train, y_train)
-    with open(get_model_path(pickle_pattern, 'K-Nearest Neighbors'), 'wb') as file:
+    with open(get_model_path(pickle_pattern, KNN_CLASSIFIER), 'wb') as file:
         pickle.dump(knn, file)
 
     nn = MLPClassifier(random_state=seed)
     train(nn, x_train, y_train)
-    with open(get_model_path(pickle_pattern, 'Neural Network'), 'wb') as file:
+    with open(get_model_path(pickle_pattern, NN_CLASSIFIER), 'wb') as file:
         pickle.dump(nn, file)
 
 
     # Save the models evaluation in a file
     if model_evaluation:    
-        predictions_dt = predict(get_model_path(pickle_pattern, 'Decision Tree'), x_test)
-        plot_confusion_matrix(y_test, predictions_dt, pickle_pattern, 'Decision Tree')
+        predictions_dt, predictions_prob_dt = predict(get_model_path(pickle_pattern, DT_CLASSIFIER), x_test)
+        plot_confusion_matrix(y_test, predictions_dt, pickle_pattern, DT_CLASSIFIER)
+        plot_roc_curve(y_test, predictions_prob_dt, pickle_pattern, DT_CLASSIFIER)
 
-        predictions_rf = predict(get_model_path(pickle_pattern, 'Random Forest'), x_test)
-        plot_confusion_matrix(y_test, predictions_rf, pickle_pattern, 'Random Forest')
+        predictions_rf, predictions_prob_rf = predict(get_model_path(pickle_pattern, RF_CLASSIFIER), x_test)
+        plot_confusion_matrix(y_test, predictions_rf, pickle_pattern, RF_CLASSIFIER)
+        plot_roc_curve(y_test, predictions_prob_rf, pickle_pattern, RF_CLASSIFIER)
 
-        predictions_lr = predict(get_model_path(pickle_pattern, 'Linear Regression'), x_test)
-        predictions_lr = (predictions_lr > 0.5).astype(int).ravel()
-        plot_confusion_matrix(y_test, predictions_lr, pickle_pattern, 'Linear Regression')
+        predictions_lr, predictions_prob_lr = predict(get_model_path(pickle_pattern, LR_CLASSIFIER), x_test)
+        plot_confusion_matrix(y_test, predictions_lr, pickle_pattern, LR_CLASSIFIER)
+        plot_roc_curve(y_test, predictions_prob_lr, pickle_pattern, LR_CLASSIFIER)
 
-        predictions_svm = predict(get_model_path(pickle_pattern, 'Support Vector Machine'), x_test)
-        plot_confusion_matrix(y_test, predictions_svm, pickle_pattern, 'Support Vector Machine')
+        predictions_svm, predictions_prob_svm = predict(get_model_path(pickle_pattern, SVM_CLASSIFIER), x_test)
+        plot_confusion_matrix(y_test, predictions_svm, pickle_pattern, SVM_CLASSIFIER)
+        plot_roc_curve(y_test, predictions_prob_svm, pickle_pattern, SVM_CLASSIFIER)
 
-        predictions_knn = predict(get_model_path(pickle_pattern, 'K-Nearest Neighbors'), x_test)
-        plot_confusion_matrix(y_test, predictions_knn, pickle_pattern, 'K-Nearest Neighbors')
+        predictions_knn, predictions_prob_knn = predict(get_model_path(pickle_pattern, KNN_CLASSIFIER), x_test)
+        plot_confusion_matrix(y_test, predictions_knn, pickle_pattern, KNN_CLASSIFIER)
+        plot_roc_curve(y_test, predictions_prob_knn, pickle_pattern, KNN_CLASSIFIER)
 
-        predictions_nn = predict(get_model_path(pickle_pattern, 'Neural Network'), x_test)
-        plot_confusion_matrix(y_test, predictions_nn, pickle_pattern, 'Neural Network')
+        predictions_nn, predictions_prob_nn = predict(get_model_path(pickle_pattern, NN_CLASSIFIER), x_test)
+        plot_confusion_matrix(y_test, predictions_nn, pickle_pattern, NN_CLASSIFIER)
+        plot_roc_curve(y_test, predictions_prob_nn, pickle_pattern, NN_CLASSIFIER)
 
         # Save the evaluation in a file
         with open(AIMODELS_FOLDER + pickle_pattern + '/' + pickle_pattern + '_evaluation.txt', 'w') as file:
@@ -171,20 +186,20 @@ def train_all(data, pickle_pattern, model_evaluation = False, test_percentage=0.
             file.write('- Training size = ' + str(train_size) + '\n')
             file.write('- Test size = ' + str(test_size) + '\n')
 
-            file.write(print_model_metrics('Decision Tree', *calculate_metrics(y_test, predictions_dt)))
+            file.write(print_model_metrics(DT_CLASSIFIER, *calculate_metrics(y_test, predictions_dt, predictions_prob_dt)))
             file.write('\n')
-            file.write(print_model_metrics('Random Forest', *calculate_metrics(y_test, predictions_rf)))
+            file.write(print_model_metrics(RF_CLASSIFIER, *calculate_metrics(y_test, predictions_rf, predictions_prob_rf)))
             file.write('\n')
-            file.write(print_model_metrics('Linear Regression', *calculate_metrics(y_test, predictions_lr)))
+            file.write(print_model_metrics(LR_CLASSIFIER, *calculate_metrics(y_test, predictions_lr, predictions_prob_lr)))
             file.write('\n')
-            file.write(print_model_metrics('Support Vector Machine', *calculate_metrics(y_test, predictions_svm)))
+            file.write(print_model_metrics(SVM_CLASSIFIER, *calculate_metrics(y_test, predictions_svm, predictions_prob_svm)))
             file.write('\n')
-            file.write(print_model_metrics('K-Nearest Neighbors', *calculate_metrics(y_test, predictions_knn)))
+            file.write(print_model_metrics(KNN_CLASSIFIER, *calculate_metrics(y_test, predictions_knn, predictions_prob_knn)))
             file.write('\n')
-            file.write(print_model_metrics('Neural Network', *calculate_metrics(y_test, predictions_nn)))
+            file.write(print_model_metrics(NN_CLASSIFIER, *calculate_metrics(y_test, predictions_nn, predictions_prob_nn)))
             file.write('\n')
 
-def predict(model_path, x_test):
+def predict(model_path, x_test) -> Tuple[list, list]:
     """
     Make predictions.
 
@@ -200,10 +215,11 @@ def predict(model_path, x_test):
 
     # Make predictions
     predictions = model.predict(x_test)
+    predictions_prob = model.predict_proba(x_test)[:, 1]
     
-    return predictions
+    return predictions, predictions_prob
 
-def calculate_metrics(y_test: list, predictions: list)-> Tuple[float, float, float, float]:
+def calculate_metrics(y_test: list, predictions: list, predictions_prob: list)-> Tuple[float, float, float, float, float]:
     """
     Calculate the metrics.
 
@@ -219,8 +235,10 @@ def calculate_metrics(y_test: list, predictions: list)-> Tuple[float, float, flo
     precision = precision_score(y_test, predictions)
     recall = recall_score(y_test, predictions)
     f1 = f1_score(y_test, predictions)
+    auc = roc_auc_score(y_test, predictions_prob)
 
-    return cm, acc, precision, recall, f1
+
+    return cm, acc, precision, recall, f1, auc
 
 def plot_confusion_matrix(y_test: list, predictions: list, pickle_pattern: str, classifier_type: str)-> None:
     """
@@ -230,6 +248,31 @@ def plot_confusion_matrix(y_test: list, predictions: list, pickle_pattern: str, 
     y_test (DataFrame): The actual target values.
     predictions (DataFrame): The predicted target values.
     """
+    plt.figure()
     ConfusionMatrixDisplay.from_predictions(y_test, predictions)
     # Save the plot to a file
     plt.savefig(AIMODELS_FOLDER + pickle_pattern + '/' + 'cm_' + parse_classifier(classifier_type) + '.png')
+    plt.close()
+
+def plot_roc_curve(y_test: list, predictions_prob: list, pickle_pattern: str, classifier_type: str)-> None:
+    """
+    Plot the ROC curve.
+
+    Args:
+    y_test (DataFrame): The actual target values.
+    predictions_prob (DataFrame): The predicted probabilities.
+    """
+    roc_auc = roc_auc_score(y_test, predictions_prob)
+    fpr, tpr, _ = roc_curve(y_test, predictions_prob)
+    plt.figure()
+    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.legend(loc="lower right")
+    # Save the plot to a file
+    plt.savefig(AIMODELS_FOLDER + pickle_pattern + '/' + 'roc_' + parse_classifier(classifier_type) + '.png')
+    plt.close()
