5a381dab09935a16f8f1ce0835b4dc048699b01d
Joaquín Alejandro España Sánchez <alexespsan@uma.es>
2024-07-03 20:55:56 +0200

    🔥 Remove boilerplate code and minor changes #14

--- project/frontend/src/app/app.routes.ts
+++ project/frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AddRepositoryComponent } from './add-repository/add-repository.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent },
+];
